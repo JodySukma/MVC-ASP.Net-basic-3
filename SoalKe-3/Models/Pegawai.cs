070eeec20ea445981a85da86f541d7cc2c879eb5
lJodyl <zody.kyozue@gmail.com>
2019-04-22 15:18:43 +0700

    first commit

---
+++ SoalKe-3/Models/Pegawai.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoalKe_3.Models
+{
+    public class Pegawai
+    {
+        private int _golongan = 1;
+        public string Nama { get; set; }
+
+        public int Golongan {
+            get { return _golongan; }
+            set { }
+        }
+    }
+}
